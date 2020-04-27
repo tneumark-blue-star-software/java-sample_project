91a51d0ddc66aa6975a2a100bf13c8fdf7e95d93
tneumark-blue-star-software <thomas.neumark@blue-star-software.com>
2020-04-27 11:06:19 -0400

    first

---
+++ project2020/src/main/java/com/bluestarsoftware/demo/Project2020Application.java
@@ -0,0 +1,13 @@
+package com.bluestarsoftware.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2020Application.class, args);
+	}
+
+}
---
+++ project2020/src/main/java/com/bluestarsoftware/demo/SampleController.java
@@ -0,0 +1,14 @@
+package com.bluestarsoftware.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	@GetMapping("sample/hello")
+	public String sayHello() {
+		return "hello: " + System.currentTimeMillis();
+	}
+	
+	
+}
